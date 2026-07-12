0feec19097acb3315440e1140628640ffa404e95
cosmeggo <cosmeggo@gmail.com>
2026-07-12 23:55:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hatchet
+A Balatro mod made for Artfight 2026!
+
+This mod requires [Steamodded](https://github.com/Steamodded/smods/releases/latest).
+
+# Current Features
+- 46 Jokers
+
+## [Discord Server](https://discord.gg/hGwkjwUV9V)
+
+## Known Bugs
+- The **Dramaturgy Deck** has not been fully implemented as of yet.
+- Berry sometimes does not upgrade after a purchase has been made.
---
+++ main.lua
@@ -0,0 +1,89 @@
+PW = SMODS.current_mod
+
+--ATLASES
+SMODS.Atlas({
+    key = "modicon", 
+    path = "ModIcon.png", 
+    px = 34,
+    py = 34,
+    atlas_table = "ASSET_ATLAS"
+})
+
+SMODS.Atlas({
+    key = "balatro", 
+    path = "balatro.png", 
+    px = 333,
+    py = 216,
+    prefix_config = { key = false },
+    atlas_table = "ASSET_ATLAS"
+})
+
+
+SMODS.Atlas({
+    key = "PWJokers", 
+    path = "PWJokers.png", 
+    px = 71,
+    py = 95, 
+    atlas_table = "ASSET_ATLAS"
+})
+
+SMODS.Atlas({
+    key = "PWBoosters", 
+    path = "PWBoosters.png", 
+    px = 71,
+    py = 95, 
+    atlas_table = "ASSET_ATLAS"
+})
+
+SMODS.Atlas({
+    key = "PWDecks", 
+    path = "PWDecks.png", 
+    px = 71,
+    py = 95, 
+    atlas_table = "ASSET_ATLAS"
+})
+
+--loading src files
+assert(SMODS.load_file("src/boosters.lua"))()
+assert(SMODS.load_file("src/challenges.lua"))()
+assert(SMODS.load_file("src/jokers.lua"))()
+assert(SMODS.load_file("src/rarities.lua"))()
+-- assert(SMODS.load_file("src/backs.lua"))() / currently broken
+
+--- Main Menu Colours (Credit to Cryptid and JoyousSpring)
+local game_main_menu_ref = Game.main_menu
+function Game:main_menu(change_context)
+    local ret = game_main_menu_ref(self, change_context)
+
+        local colours = { c1 = HEX("2287c3"), c2 = HEX("8ef3ff") }
+        G.SPLASH_BACK:define_draw_steps({
+            {
+                shader = "splash",
+                send = {
+                    { name = "time",       ref_table = G.TIMERS, ref_value = "REAL_SHADER" },
+                    { name = "vort_speed", val = 0.4 },
+                    { name = "colour_1",   ref_table = colours,  ref_value = "c1" },
+                    { name = "colour_2",   ref_table = colours,      ref_value = "c2" },
+                },
+            },
+        })
+    return ret
+end
+
+-- SMODS Optional Features
+SMODS.current_mod.optional_features = {
+    retrigger_joker = true,
+    post_trigger = true,
+    quantum_enhancements = true,
+    cardareas = {
+        discard = true,
+        deck = true
+    }
+}
+
+-- Credit to NopeTooFast
+SMODS.current_mod.menu_cards = function()
+return {
+  { key = 'j_pw_wormston' },
+  remove_original = true
+} end
